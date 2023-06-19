679f797e397483cbcf178df9bed99714303856da
Ayed Naber <naberayed@gmail.com>
2023-06-18 22:28:09 -0400

    Added video to call to action

--- pages/components/CallToAction/CallToAction.js
+++ pages/components/CallToAction/CallToAction.js
@@ -11,13 +11,22 @@ const CallToAction = () => {
   };
 
   return (
-    <section className={styles.ctaSection}>
-      <h2>Experience the Power of SincerelyAI Today!</h2>
-      <p>Transform your communication style and make your professional interactions seamless and efficient. Try out our AI-powered writing assistant now!</p>
-      <button onClick={handleClick} className={styles.demoButton}>
-        Try the Demo
-      </button>
-    </section>
+    <div className={styles.videoBackground}>
+      <video autoPlay={true} loop={true} muted={true} className={styles.vid}>
+        <source src='/writingemailvid.mp4' type='video/mp4' />
+        Your browser does not support.
+      </video>
+      <div className={styles.overlay} />
+      <div className={styles.content}>
+        <div className={styles.contentElements}>
+          <h2>Experience the Power of SincerelyAI Today!</h2>
+          <p>Transform your communication style and make your professional interactions seamless and efficient. Try out our AI-powered writing assistant now!</p>
+          <button onClick={handleClick} className={styles.demoButton}>
+            Try the Demo
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
